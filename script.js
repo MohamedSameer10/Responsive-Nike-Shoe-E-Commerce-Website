16d25bea4617795e7fd3ad39d4448ad3a4b0b448
MohamedSameer10 <mohamedsameer.syed@gmail.com>
2024-01-26 16:28:31 +0530

    added the project

--- script.js
+++ script.js
@@ -55,10 +55,10 @@ searchBar.addEventListener("keyup",()=>{
 window.addEventListener("scroll",()=>{
     quality.forEach((element)=>{
         element.classList.toggle("scroll",window.scrollY>1010)
-        element.classList.toggle("responsivescroll",window.scrollY>1100)
+        element.classList.toggle("responsivescroll",window.scrollY>1400)
     })
     season.classList.toggle("smooth",window.scrollY>1600)
-    season.classList.toggle("responsivesmooth",window.scrollY>1900)
+    season.classList.toggle("responsivesmooth",window.scrollY>2100)
 })
 
 
